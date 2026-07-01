fb445e3c7ee779eaf3ecfd8ad4870fc380ffa3f3
jihun8861 <ji55hun@oasis.inje.ac.kr>
2026-07-01 18:53:46 +0900

    feat: intercepter 추가

--- src/main/java/org/example/board/config/WebSocketConfig.java
+++ src/main/java/org/example/board/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*").withSockJS();;
+        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*").addInterceptors(stompHandshakeInterceptor).withSockJS();;
     }
 
     @Override
